8e0dcb20ee936f4ec7537b4e52b43a4340955919
seankearon <sean@tackaberry.org>
2012-05-23 15:47:41 +0100

    First drop

---
+++ Raven Obfuscation/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Raven.Client.Document;
+using Raven.Client.Embedded;
+using Raven.Client.Indexes;
+
+namespace Raven_Obfuscation
+{
+    public partial class Form1 : Form
+    {
+        private readonly EmbeddableDocumentStore store;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            store = new EmbeddableDocumentStore { UseEmbeddedHttpServer = true };
+            store.Initialize();
+
+            var definition = new PeopleByName {Conventions = new DocumentConvention()}.CreateIndexDefinition();
+            MessageBox.Show(definition.Map);
+
+            QueryOverTheIndex();
+        }
+
+        private void QueryOverTheIndex()
+        {
+            using (var session = store.OpenSession())
+            {
+                Text = "Number of Seans = " + session.Query<Person, PeopleByName>().Customize(x => x.WaitForNonStaleResults()).Where(x => x.Name == "Sean").Take(100).Count();
+            }
+        }
+
+        private void AddPerson()
+        {
+            using (var session = store.OpenSession())
+            {
+                session.Store(new Person{Name = "Sean" });
+                session.SaveChanges();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddPerson();
+            QueryOverTheIndex();
+        }
+    }
+
+    public class PeopleByName: AbstractIndexCreationTask<Person>
+    {
+        public PeopleByName()
+        {
+            Map = persons =>
+                  from p in persons
+                  select new
+                             {
+                                 p.Name
+                             };
+        }
+    }
+
+    [Obfuscation(Exclude = true)]
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
